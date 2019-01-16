a4a01072b62aaecf4e5062d7b7bc9aec3b502fd1
zhangIrene07 <46522559+zhangIrene07@users.noreply.github.com>
2019-01-16 13:02:57 +0000

    Add files via upload

---
+++ AssEx2.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+public class AssEx2 {
+	public static void main(String[] args)throws IOException{
+       MonoCipher m=new MonoCipher();
+       m.RunMonoCipher();
+     //VigenereCipher v=new VigenereCipher();
+     //v.RunVigeCipher();
+    }
+}
