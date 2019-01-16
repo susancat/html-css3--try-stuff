0b3dbf522092364ae4de3326bce5d5aa31ec9d2d
bmoyler <46538994+bmoyler@users.noreply.github.com>
2019-01-16 13:23:04 +0000

    Delete AssEx2.java

--- AssEx2.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-public class AssEx2 {
-	public static void main(String[] args)throws IOException{
-       MonoCipher m=new MonoCipher();
-       m.RunMonoCipher();
-     //VigenereCipher v=new VigenereCipher();
-     //v.RunVigeCipher();
-    }
-}
